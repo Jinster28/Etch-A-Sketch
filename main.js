2122ed62df98c76c1276f7ddee401d3c19139601
Simon Jin <jinster28@gmail.com>
2023-04-05 15:52:39 -0400

    Created a general structure for main.js  Also started createSqr function to generate squares

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 // Main.js - Etch-A-Sketch
+
+// GLOBAL VARIABLE DECLARATIONS
+
+
+// FUNCTIONS
+
+// Function that takes a parameter of number of Squares to generate
+// if num is invalid, assume 1 square
+function createSqr(num) {
+
+    let sqrDiv;
+
+    for (i = 0; i < num, i++) {
+
+    }
+}
